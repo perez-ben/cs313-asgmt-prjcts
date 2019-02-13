308b032d232bef93a7ed61cf5797be74cc1211ee
Ben Perez <per15066@byui.edu>
2019-02-13 02:29:30 -0800

    I added the basic information required by the assignment.

--- web/index.php
+++ web/index.php
@@ -1,4 +1,16 @@
 <?php include ("header.php") ?>
 
-<img src="
+      <img src="trees.jpg" alt="branches"
+      <div>
+	<h1>Roots, Trunks, and Branches</h1>
+	<p> Family history is a very important part of our identity. Many
+	people go through life without ever really knowing where their 
+	family originates. Family history has become a big part of my life
+	over the past decade. Understanding where my ancestors originate 
+	can help answer many questions. I hope to identify some good resources
+	to help others find their roots so they can be a trunk that grows
+	branches.</p>
+
+</div> 
 	
+<?php include ("footer.php"); ?>
