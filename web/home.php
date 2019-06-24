5b2683dca752fbd286f8c8e1adba9649f4e9cd41
Ben Perez <per15066@byui.edu>
2019-06-23 20:44:51 -0700

    nav part 1
    created the list and initial layout

--- web/home.php
+++ web/home.php
@@ -8,7 +8,17 @@
             <div class="">
             <h1>Coursework and Projects</h1>
             </div>
+            <nav>
+                <ul class="nav">
+                    <li class="link"<a href="web_engineering/engineering_1.php">CS 213 Front End</a></li>
+                    <li class="link"><a href="web_engineering/engineering_2.php">CS 313 Back End</a></li>
+                    <li class="link"></li>
+                    <li class="link"></li>
+                    <li class="link"></li>
+                </ul>
+            </nav>
         </header>
+
             <div class="">
             <p>I have done my best to organize my work to demonstrate my skills and knowledge.
             Please explore the site. My goal has been to incorporate everything into one final
