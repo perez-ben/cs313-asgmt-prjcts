e2a312f332aaa886cf51ef4e8654a4f0a9f3291f
Ben Perez <per15066@byui.edu>
2019-09-21 19:01:35 -0700

    new semster

--- web/home.php
+++ web/home.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html lang="en-US">
     <style>
-    <?php include 'homepage.css';
-    ?>
+    <?php include 'homepage.css'; ?>
     </style>
-    <head>
-        <meta charset="UTF-8">
-        <title>Movie Search</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src='home.js'></script>
-    </head>
     <body>
         <header>
             <div class="">
