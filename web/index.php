7625d3e39f8d6d89fdfad151a486e166f5015c45
Ben Perez <per15066@byui.edu>
2019-09-30 00:14:49 -0700

    adding sidenav and updating look

--- web/index.php
+++ web/index.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
-<html lang=""en-US">
-<style>
-<?php include 'homepage.css'; ?>
-</style>
-<body>
+<html lang="en-US">
+    <style>
+    <link rel="stylesheet" href="homepage.css";>
+    </style>
+    <body>
+    <div id="mySidenav" class="sidenav">
+                <a href="home.php" id="home">Home</a>
+                <a href="web_engineering/engineering_1.php" id="cs213">CS 213 Front-End</a>
+                <a href="web_engineering/engineering_2.php" id="cs313">CS 313 Back-End</a>
+                <a href="#" id="projects">Projects</a>
+                <a href="#" id="contact">Contact</a>
+            </div>
+
     <div class="main_page">
         <h1>Software Engineer. Data. Solutions.</h1>
         <p> I love my job and I love working in higher education. I am always
