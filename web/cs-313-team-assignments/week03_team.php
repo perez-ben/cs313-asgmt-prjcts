2e9f01b5d328bebba85d05ecdf5ba06480ce2ee2
Ben Perez <per15066@byui.edu>
2019-10-01 20:59:08 -0700

    update

--- web/cs-313-team-assignments/week03_team.php
+++ web/cs-313-team-assignments/week03_team.php
@@ -1,26 +1,3 @@
-<?php
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$major = $_POST['input_major'];
-$comments = $_POST['comments'];
-
-echo 'Name: ' . $name . "<br>";
-echo 'Email: ' . $email . "<br>";
-echo 'Major: ' . $major . "<br>";
-echo 'Comments: ' . $comments . "<br>";
-
-if( ! empty( $_POST['input_continent'] ) ) {
-    echo 'Continents visited: ';
-    // Loop to store and display values of individual checked checkbox.
-    foreach ( $_POST['input_continent'] as $selected ) {
-        echo $selected."<br>";
-    }
-}
-
-$majors = ['Computer Science', 'Web Design and Development', 'Computer Information Technology', 'Computer Engineering'];
-?>
-
 <!DOCTYPE html>
 <html lang="en-US">
 
@@ -30,25 +7,28 @@ $majors = ['Computer Science', 'Web Design and Development', 'Computer Informati
 
 <body>
     <form action="" method="POST">
-
+        <br><br>
         <label for="input_name">Name</label>
         <input id="input_name" type="text" name="name" placeholder="Name"> <br>
 
+        <br><br>
         <label for="input_email">Email</label>
         <input id="input_email" type="text" name="email" placeholder="Email"> <br>
 
         <?php if ( ! empty( $majors )) { ?>
+        <br><br>
         <label for="radio_major">Major</label>
         <div id="radio_major"></div>
         <?php foreach ( $majors as $major ) { ?>
             <input type="radio" name="input_major" value="<?php echo $major; ?>"><?php echo $major; ?><br>
         <?php } ?>
         </div>
-        <br>
         <?php } ?>
 
+        <br><br>
         <textarea name="comments" placeholder="Comments" id="comments" cols="30" rows="10"></textarea> <br>
         
+        <br><br>
         <label for="checkbox_list">What continents have you been to?</label>
         <div id="checkbox_list"></div>
             <input type="checkbox" value="na" name="input_continent[]">North America <br>
@@ -60,8 +40,36 @@ $majors = ['Computer Science', 'Web Design and Development', 'Computer Informati
             <input type="checkbox" value="ant" name="input_continent[]">Antarctica <br>
         </div> <br>
 
+        <hr>
+
         <button type="submit">Submit</button>
     </form>
+    
+    <hr>
+    <div id="form-results">
+        <h2>Form Results</h2>
+    <?php
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $major = $_POST['input_major'];
+        $comments = $_POST['comments'];
+
+        echo 'Name: ' . $name . "<br>";
+        echo 'Email: ' . $email . "<br>";
+        echo 'Major: ' . $major . "<br>";
+        echo 'Comments: ' . $comments . "<br><br>";
+
+        if( ! empty( $_POST['input_continent'] ) ) {
+            echo 'Continents visited:<br>';
+            // Loop to store and display values of individual checked checkbox.
+            foreach ( $_POST['input_continent'] as $selected ) {
+                echo $selected."<br>";
+            }
+        }
+
+        $majors = ['Computer Science', 'Web Design and Development', 'Computer Information Technology', 'Computer Engineering'];
+        ?>
+    </div>
 </body>
 
 </html>
