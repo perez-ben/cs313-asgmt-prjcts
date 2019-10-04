a6d123d763be4932705b09ea7c4628a60cfae087
Ben Perez <per15066@byui.edu>
2019-10-03 18:15:12 -0700

    update

--- web/home.php
+++ web/home.php
@@ -8,8 +8,8 @@
         <div class="title">
             <h1>Coursework and Projects</h1>
         </div>
-        <div id="myNav" class="sidenav">
-            <a href="home.php" id="home">Home</a>
+        <div id="myNav" class="topnav">
+            <a href="index.php" id="home">Home</a>
             <a href="web_engineering/engineering_1.php" id="cs213">CS 213 Front-End</a>
             <a href="web_engineering/engineering_2.php" id="cs313">CS 313 Back-End</a>
             <a href="#" id="projects">Projects</a>
--- web/index.php
+++ web/index.php
@@ -4,8 +4,8 @@
     <link rel="stylesheet" href="homepage.css";>
 </head>
 <body>
-    <div id="mySidenav" class="sidenav">
-        <a href="home.php" id="home">Home</a>
+    <div id="myNav" class="topNav">
+        <a href="index.php" id="home">Home</a>
         <a href="web_engineering/engineering_1.php" id="cs213">CS 213 Front-End</a>
         <a href="web_engineering/engineering_2.php" id="cs313">CS 313 Back-End</a>
         <a href="#" id="projects">Projects</a>
@@ -18,10 +18,10 @@
             looking to improve and innovate higher education technology to add
             relevant data to curriculum analytics and revolutionize institutional
             effectiveness.
+        <p>I have done my best to organize my work to demonstrate my skills and knowledge.
+            Please explore the site. My goal has been to incorporate everything into one final
+            project.</p>
         </p>
-        <div class="button_style">
-            <a href="home.php" class="enter" type="button">Explore</a>
-        </div>
     </div>
 </body>
 </html>
