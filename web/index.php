725eb3bfac9437432f7856d338fc8d8092524542
Ben Perez <per15066@byui.edu>
2019-11-13 18:52:49 -0800

    nav

--- web/index.php
+++ web/index.php
@@ -7,30 +7,32 @@
 <body>
     <div id="myNav" class="topNav">
         <a href="index.php" id="home">Home</a>
-            <div class ="dropdown">
-                <button class="dropbtn">Web Engineering I
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                 <div class="dropdown-content">
-                    <a href="#" id="cs213">Week 2</a>
-                    <a href="#" id="cs213">Week 3</a>
-                    <a href="#" id="cs213">Week 4</a>
-                    <a href="#" id="cs213">Week 5</a>
-                    <a href="#" id="cs213">Week 6</a>
-                    <a href="#" id="cs213">Week 7</a>
-                </div>
-                <button class="dropbtn">Web Engineering II
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content2">
-                    <a href="#" id="cs313">Week 2</a>
-                    <a href="#" id="cs313">Week 3</a>
-                    <a href="#" id="cs313">Week 4</a>
-                    <a href="#" id="cs313">Week 5</a>
-                    <a href="#" id="cs313">Week 6</a>
-                    <a href="#" id="cs313">Week 7</a>
-                </div>
+        <div class ="dropdown">
+            <button class="dropbtn">Web Engineering I
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+            <a href="#" id="cs213">Week 2</a>
+                <a href="#" id="cs213">Week 3</a>
+                <a href="#" id="cs213">Week 4</a>
+                <a href="#" id="cs213">Week 5</a>
+                <a href="#" id="cs213">Week 6</a>
+                <a href="#" id="cs213">Week 7</a>
             </div>
+        </div>
+        <div class ="dropdown">
+            <button class="dropbtn">Web Engineering II
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="#" id="cs313">Week 2</a>
+                <a href="#" id="cs313">Week 3</a>
+                <a href="#" id="cs313">Week 4</a>
+                <a href="#" id="cs313">Week 5</a>
+                <a href="#" id="cs313">Week 6</a>
+                <a href="#" id="cs313">Week 7</a>
+            </div>
+        </div>
         <a href="#" id="projects">Projects</a>
         <a href="#" id="contact">Contact</a>
     </div>
