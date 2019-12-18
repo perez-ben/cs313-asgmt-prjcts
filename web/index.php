2f64b32a25988a69f262a875449007d18a449703
Ben Perez <per15066@byui.edu>
2019-12-17 20:28:26 -0800

    cleanup

--- web/index.php
+++ web/index.php
@@ -1,52 +1,53 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<head>
-    <title>Ben's Homepage</title>
-    <link rel="stylesheet" href="homepage.css">
-</head>
-<body>
-    <div id="myNav" class="topNav">
-        <a href="index.php" id="home">Home</a>
-        <div class ="dropdown">
-            <button class="dropbtn">Web Engineering I
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-            <a href="#" id="cs213">Week 2</a>
-                <a href="#" id="cs213">Week 3</a>
-                <a href="#" id="cs213">Week 4</a>
-                <a href="#" id="cs213">Week 5</a>
-                <a href="#" id="cs213">Week 6</a>
-                <a href="#" id="cs213">Week 7</a>
+    <head>
+        <title>Ben's Homepage</title>
+        <link rel="stylesheet" href="homepage.css">
+    </head>
+    <body>
+        <div id="myNav" class="topNav">
+            <a href="index.php" id="home">Home</a>
+            <div class ="dropdown">
+                <button class="dropbtn">Web Engineering I
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="cs313/week02.html" id="cs213">Week 2</a>
+                    <a href="#" id="cs213">Week 3</a>
+                    <a href="#" id="cs213">Week 4</a>
+                    <a href="#" id="cs213">Week 5</a>
+                    <a href="#" id="cs213">Week 6</a>
+                    <a href="#" id="cs213">Week 7</a>
+                </div>
             </div>
-        </div>
-        <div class ="dropdown">
-            <button class="dropbtn">Web Engineering II
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <a href="cs313/week02.html" id="cs313">Week 2</a>
-                <a href="#" id="cs313">Week 3</a>
-                <a href="#" id="cs313">Week 4</a>
-                <a href="#" id="cs313">Week 5</a>
-                <a href="#" id="cs313">Week 6</a>
-                <a href="#" id="cs313">Week 7</a>
+            <div class ="dropdown">
+                <button class="dropbtn">Web Engineering II
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="cs313/week02.html" id="cs313">Week 2</a>
+                    <a href="week02_team/week02_team.html">W02 Team</a>
+                    <a href="#" id="cs313">Week 3</a>
+                    <a href="#" id="cs313">Week 4</a>
+                    <a href="#" id="cs313">Week 5</a>
+                    <a href="#" id="cs313">Week 6</a>
+                    <a href="#" id="cs313">Week 7</a>
+                </div>
             </div>
+            <a href="#" id="projects">Projects</a>
+            <a href="#" id="contact">Contact</a>
         </div>
-        <a href="#" id="projects">Projects</a>
-        <a href="#" id="contact">Contact</a>
-    </div>
 
-    <div class="main_page">
-        <h1>Software Engineer. Data. Solutions.</h1>
-        <p> I love my job and I love working in higher education. I am always
+        <div class="main_page">
+            <h1>Software Engineer. Data. Solutions.</h1>
+            <p> I love my job and I love working in higher education. I am always
             looking to improve and innovate higher education technology to add
             relevant data to curriculum analytics and revolutionize institutional
             effectiveness.
             I have done my best to organize my work to demonstrate my skills and knowledge.
             Please explore the site. My goal has been to incorporate everything into one final
             project.
-        </p>
-    </div>
-</body>
+            </p>
+        </div>
+    </body>
 </html>
